73420cf1006fc74b512507251f17c0ded25a31e4
Val Daven Obrero <valdavenobrero@gmail.com>
2024-11-21 14:42:08 +0800

    Revert "Library Backend"
    This reverts commit a8950c8fd97df2e60e4ac83a566886a45b0caaa4.

--- src/librarybackend/src/main/java/libraryManagement/librarybackend/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package libraryManagement.librarybackend.repository;
-
-import libraryManagement.librarybackend.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
